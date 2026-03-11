1708c87c92fe2d6598cbadf403a9cad2bc8c9f53
bijoy <bijoycse24@gmail.com>
2026-03-11 19:51:16 +0600

    fix

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     <div className="text-center text-2xl pt-10 border-t">
       <Title txt1={"CONTACT"} txt2={"US"} />
       <div className="my-10 mb-28 flex flex-col justify-center md:flex-row gap-10">
-        <img className="w-full md:max-w-120" src={assets.contact_img} alt="" />
+        {/* <img className="w-full md:max-w-120" src={assets.contact_img} alt="" /> */}
         <div className="flex flex-col items-start gap-6">
           <p className="font-semibold text-xl text-gray-600">Our Store</p>
           <p className="text-gray-500 text-start text-xl">
