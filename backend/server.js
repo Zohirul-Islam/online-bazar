69d73a6ec280eb236ef141b78f619a1cdb6e082a
bijoy <bijoycse24@gmail.com>
2026-04-21 14:29:31 +0600

    cart controller api is done

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
 
 // app config
 const app = express();
@@ -16,7 +17,8 @@ app.use(express.json());
 app.use(cors());
 // api endpoint
 app.use('/api/user', userRouter);
-app.use('/api/product',productRouter)
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
 app.get("/", (req, res) => {
   res.send("api is working");
 });
